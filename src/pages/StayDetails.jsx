b53feef8e794f6bf924769b2c3a6e9cea57101e0
yonatmiller <yonatmiller@gmail.com>
2026-01-08 11:31:47 +0200

    Adding icons and dividing the pagination StayDetails

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -2,6 +2,11 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { RiStarFill, RiTvLine } from "react-icons/ri";
+import { HiOutlineTv } from "react-icons/hi2";
+import { HiOutlineWifi } from "react-icons/hi";
+import { IoIosSnow } from "react-icons/io";
+import { TbToolsKitchen2, TbWindow } from "react-icons/tb";
 
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStay, addStayMsg } from '../store/actions/stay.actions'
@@ -11,6 +16,13 @@ export function StayDetails() {
 
   const { stayId } = useParams()
   const stay = useSelector(storeState => storeState.stayModule.stay)
+  const iconMap = {
+    "Wifi": <HiOutlineWifi />,
+    "Air conditioning": <IoIosSnow />,
+    "Kitchen": <TbToolsKitchen2 />,
+    "TV": <HiOutlineTv />,
+    "Balcony": <TbWindow />
+  };
 
   useEffect(() => {
     loadStay(stayId)
@@ -28,13 +40,12 @@ export function StayDetails() {
 
   return (
     <section className="stay-details">
-      <Link to="/stay">Back to list</Link>
       {stay && <div>
 
         <div class="heading flex">
           <h1>{stay.name}</h1>
           <div class="right-heading flex">
-            <p>save</p>
+            <p>♡ save</p>
             <p>share</p>
           </div>
         </div>
@@ -49,36 +60,52 @@ export function StayDetails() {
             <div><img src={stay.imgUrl} alt={stay.name} class="bottom-right" /></div>
           </div>
         </div>
-        <div class="description">
+        <div class="description third-page">
           <h2>
-            {stay.type}{stay.name}
+            {stay.type} {stay.name}
           </h2>
-          <p class="guests">{stay.capacity} guests-{stay.capacity / 2} bedroom</p>
-          <h3>{stay.rate} - {stay.reviews.length}</h3>
-          <p>{stay.description}</p>
+          <p class="guests">{stay.capacity} guests · {stay.capacity / 2} bedroom</p>
+          <h3>
+            <div className="meta-item">
+              <RiStarFill size={10} />
+              {stay.rate} · {stay.reviews.length} reviews
+
+            </div>
+          </h3>
         </div>
-        <div className="amenities">
+        <div class="third-page">
+          <div className="divider"></div>
+          <p class="description-p">{stay.description}</p>
+        </div>
+        <div className="amenities third-page">
+          <div className="divider"></div>
+          <h2>What this place offers</h2>
           {stay.amenities && (
             <>
               {(() => {
-                // חישוב אמצע המערך
                 const mid = Math.ceil(stay.amenities.length / 2);
                 const queue1 = stay.amenities.slice(0, mid);
                 const queue2 = stay.amenities.slice(mid);
 
                 return (
-                  <div className="amenities-wrapper" style={{ display: 'flex', gap: '40px' }}>
-                    {/* תור ראשון */}
+                  <div
+                    className="amenities-wrapper"
+                    style={{ display: "flex", gap: "40px" }}
+                  >
                     <div className="queue1">
                       {queue1.map((item, idx) => (
-                        <div key={idx}>{item}</div>
+                        <div key={idx} className="amenity-item">
+                          {iconMap[item] || null}
+                          <span> {item} </span>
+                        </div>
                       ))}
                     </div>
-
-                    {/* תור שני */}
                     <div className="queue2">
                       {queue2.map((item, idx) => (
-                        <div key={idx}>{item}</div>
+                        <div key={idx} className="amenity-item">
+                          {iconMap[item] || null}
+                          <span> {item} </span>
+                        </div>
                       ))}
                     </div>
                   </div>
@@ -87,6 +114,7 @@ export function StayDetails() {
             </>
           )}
         </div>
+
 
 
         {/* <pre> {JSON.stringify(stay, null, 2)} </pre> */}
