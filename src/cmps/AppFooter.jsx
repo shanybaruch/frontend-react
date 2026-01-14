18fd84e31cd18daed27a09962b71720214ea04f4
Almog Mizrahi <almogosh111@gmail.com>
2026-01-14 18:56:52 +0200

    change footer css design

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -3,7 +3,6 @@ import { SvgIcon } from './SvgIcon'
 
 
 export function AppFooter() {
-	const count = useSelector(storeState => storeState.userModule.count)
 
 	return (
 		<footer className="app-footer full">
@@ -13,25 +12,17 @@ export function AppFooter() {
 				<a href='https://he.airbnb.com/help/article/2855'>Privacy</a>
 				<span>·</span>
 				<a href='https://he.airbnb.com/help/article/2908'>Terms</a>
-				<span>·</span>
 			</div>
 			
-			{count}
-			
-            {import.meta.env.VITE_LOCAL ? 
-                <span className="local-services">Local Services</span> : 
-                <span className="remote-services">Remote Services</span>
-			}
+		
 
             <div className="footer-right">
-				<p> ₪ ILS </p>
-				<span>·</span>
-				<p> <SvgIcon iconName="globe" /> English</p>
-				<span>·</span>
-		        <a href='https://www.instagram.com/airbnb/'><SvgIcon iconName="instagram" /></a>
-				<span>·</span>
-		        <a href="https://github.com/shanybaruch/frontend-react.git "><SvgIcon iconName="git" /></a>
-				<span>·</span>
+				<ul>
+					<li> ₪ ILS </li>
+					<li> <SvgIcon iconName="globe" /> English </li>
+					<li> <a href='https://www.instagram.com/airbnb/'><SvgIcon iconName="instagram" /></a> </li>
+					<li> <a href="https://github.com/shanybaruch/frontend-react.git "><SvgIcon iconName="git" /></a> </li>
+				</ul>
 			</div>
 		</footer>
 	)
